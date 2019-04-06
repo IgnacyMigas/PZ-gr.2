3faecd14c8e617d644bce328f5fbaa6e3955cd42
Udiknedormin <udiknedormin@gmail.com>
2019-04-06 21:51:20 +0200

    add router

---
+++ client/src/components/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Strona Główna</h1>
+
+    Witamy na stronie głównej.
+  </div>
+</template>
+
+<script>
+
+/**
+ * Strong główna serwisu.
+ *
+ * Używana bezpośrednio przez router.
+ *
+ * @module components/views/Home
+ */
+export default {
+  name: 'home'
+}
+</script>
