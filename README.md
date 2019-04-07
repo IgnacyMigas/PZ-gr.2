2a4691c410a8afb0751461090fcb133fd5f7b2af
Adam K <amkurzak@gmail.com>
2019-04-07 22:52:28 +0200

    uml diagrams

--- README.md
+++
@@ -1,2 +0,0 @@
-# PZ-gr.2
-[![Build Status](https://travis-ci.org/IgnacyMigas/PZ-gr.2.svg?branch=master)](https://travis-ci.org/IgnacyMigas/PZ-gr.2)
--- mydocker/app.js
+++
@@ -1,9 +0,0 @@
-//Przkładowa aplikacja klienta zwracająca coś (tutaj wersje noda)
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send( { version: process.version } ));
-//app.get('/', (req, res) => res.send('Elo makrelo!'));
-
-app.listen(8080, () => console.log('Server is up!'));
