b09b4835abffb28371618a56e52b9c36094bb282
Udiknedormin <udiknedormin@gmail.com>
2019-04-07 05:27:46 +0200

    add mock search bar

--- client/src/components/views/List.vue
+++ client/src/components/views/List.vue
@@ -1,43 +1,63 @@
 <template>
   <page title="Lista">
     Tu będzie lista wyszukiwania.
-    <get-list
-      title='Metryki'
-      :tryGet="tryListMetrics">
 
-      <p class='log'>{{ log }}</p>
-      <p class='error' v-for="err in error" :key="err">
-        {{ err }}
-      </p>
+    <v-container fluid fill-height>
+      <v-layout justify-center>
+        <v-flex xs18 sm12 md6>
+          <v-card class="elevation-12">
+            <v-card-text>
+              <search-bar
+                @click:append="toggleMenu"
+                :handler="saySearch"
+              />
+              <span v-if="searched">
+                Szukano: ,,{{ searched }}''
+              </span>
+            </v-card-text>
+          </v-card>
+          <v-card class="elevation-12">
+            <get-list
+              title='Metryki'
+              :tryGet="tryListMetrics">
 
-      <v-data-table
-        :headers="headers"
-        :items="metrics"
-        hide-actions
-        class="elevation-1"
-        no-data-text='Brak metryk do wyświetlenia'
-      >
-        <template slot="items" slot-scope="{ item }">
-          <td align="left">
-            {{ item.name }}
-          </td>
-          <td align="right" class="caption">
-            ( {{ item.type }} )
-          </td>
-          <td>
-            <bar-button
-             icon="list"
-             handler="showRecords(item)" />
-            <bar-button
-             icon="add_circle"
-             handler="addMetricBy(item)" />
-            <bar-button
-             icon="show_chart"
-             handler="addToChart(item)" />
-          </td>
-        </template>
-      </v-data-table>
-    </get-list>
+              <p class='log'>{{ log }}</p>
+              <p class='error' v-for="err in error" :key="err">
+                {{ err }}
+              </p>
+
+              <v-data-table
+                :headers="headers"
+                :items="metrics"
+                hide-actions
+                class="elevation-1"
+                no-data-text='Brak metryk do wyświetlenia'
+              >
+                <template slot="items" slot-scope="{ item }">
+                  <td align="left">
+                    {{ item.name }}
+                  </td>
+                  <td align="right" class="caption">
+                    ( {{ item.type }} )
+                  </td>
+                  <td>
+                    <bar-button
+                     icon="list"
+                     :handler="() => showRecords(item)" />
+                    <bar-button
+                     icon="add_circle"
+                     :handler="() => addMetricBy(item)" />
+                    <bar-button
+                     icon="show_chart"
+                     :handler="() => addToChart(item)" />
+                  </td>
+                </template>
+              </v-data-table>
+            </get-list>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </page>
 </template>
 
@@ -45,6 +65,7 @@
 import Page from '@/components/templates/Page'
 import GetList from '@/components/sections/GetList'
 import BarButton from '@/components/elements/BarButton'
+import SearchBar from '@/components/elements/SearchBar'
 
 /**
  * Strona listy zasobów.
@@ -58,10 +79,12 @@ export default {
   components: {
     'page': Page,
     'get-list': GetList,
-    'bar-button': BarButton
+    'bar-button': BarButton,
+    'search-bar': SearchBar
   },
   data () {
     return {
+      searched: '',
       metrics: [],
       headers: [
         {
@@ -88,6 +111,11 @@ export default {
     }
   },
   methods: {
+
+    saySearch (text) {
+      this.searched = text
+    },
+
     /** Pobiera dane do wylistowania metryk. */
     tryListMetrics: async function () {
       const data = [
@@ -136,6 +164,11 @@ export default {
     /** Dodaje do wykresu */
     addToChart (item) {
       item
+      //TODO
+    },
+
+    /** Pokazuje lub ukrywa menu wyszukiwania */
+    toggleMenu () {
       //TODO
     }
   }
