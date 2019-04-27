c9aeb8dadbf1372f5d56b613a175f54f6859ceef
Ignacy Migas <gigi.migas@gmail.com>
2019-04-27 23:29:30 +0200

    Add database

--- APIGateway/gateway/models.py
+++ APIGateway/gateway/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Monitor(models.Model):
     id = models.TextField(primary_key=True, unique=True)
     endpoint = models.TextField()
