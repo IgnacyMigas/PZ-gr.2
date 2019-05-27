3f1c589876894626be141301fa6eaa607663c6cc
Adam K <amkurzak@gmail.com>
2019-05-28 00:02:01 +0200

    sensor registration args

--- Sensor/source/sensor.py
+++ Sensor/source/sensor.py
@@ -1,19 +1,66 @@
 import timer as ti
 import time
 from tools import SensorTools
+import argparse
 
-def sensor_main(tool):
-    tool.register_sensor()
-    # TODO: check if registration OK. If not try to register again !!!
+parser = argparse.ArgumentParser()
+parser.add_argument('--reg',                 '-r',  help="registration flag. Options:yes or no. Default: yes",             type=str,   default="yes")
+parser.add_argument('--name',                '-n',  help="sensor name",                                                    type=str,   default="pokojXXX")
+parser.add_argument('--interval',            '-i',  help="measurements interval. Min: 1.0. Max: 30.0",                     type=float, default=1.0)
+parser.add_argument('--measurements_amount', '-ma', help="number of measurements sended in one package. Min: 1. Max: 100", type=int,   default=10)
+parser.add_argument('--metrics',             '-m',  help="metrics flag. Options: CPU, Battery or Both, Default: Both",     type=str,   default="Both")
+
+def check_starting_arguments(args):
+    args.reg                 = check_reg_argument(args.reg)
+    args.interval            = check_interval_argument(args.interval)
+    args.measurements_amount = check_measurements_amount_argument(args.measurements_amount)
+    args.metrics             = check_metrics_argument(args.metrics)
+    return args
+
+def check_reg_argument(reg):
+    if (reg != "yes") and (reg != "no"):
+        print("Invalid reg option. Sensor will be run with default value: yes")
+        reg = "yes"
+    return reg
+
+def check_interval_argument(interval):
+    if (interval < 1.0):
+        print("Interval can not be lower than 1.0. Sensor will be run with interval min value: 1.0")
+        interval = 1.0
+    elif (interval > 30.0):
+        print("Interval can not be higher than 30.0. Sensor will be run with interval max value: 30.0")
+        interval = 30.0
+    return interval
+
+def check_measurements_amount_argument(measurements_amount):
+    if (measurements_amount < 1):
+        print("Measurements amount can not be lower than 1. Sensor will be run with measurements amount min value: 1")
+        measurements_amount = 1
+    elif (measurements_amount > 100):
+        print("Measurements amount can not be higher than 100. Sensor will be run with measurements amount max value: 100")
+        measurements_amount = 100
+    return measurements_amount
+
+def check_metrics_argument(metrics):
+    if (metrics != "CPU") and (metrics != "Battery") and (metrics != "Both"):
+        print("Invalid metrics option. Sensor will be run with default value: Both")
+        metrics = "Both"
+    return metrics
+        
+def sensor_main(args):
+    tool = SensorTools(args.metrics, args.name)
+    if args.reg == "yes":
+        tool.register_sensor()
     
-    timer = ti.Timer(interval=1.0, function=tool.colect_data)
+    timer = ti.Timer(interval=args.interval, function=tool.colect_data)
     
+    timer.start()
     while True:
-        timer.start()
-        time.sleep(10.0)
-        timer.stop()
+        time.sleep(args.measurements_amount*args.interval)
+        #timer.stop()
         tool.send_data()
     
 if __name__ == '__main__':
-    tool = SensorTools("Both")
-    sensor_main(tool)
+    args = parser.parse_args()
+    args = check_starting_arguments(args)
+    sensor_main(args)
--- Sensor/source/tools.py
+++ Sensor/source/tools.py
@@ -2,41 +2,42 @@ import requests
 import psutil
 import datetime
 import time
+import platform
 
 class SensorTools:
-    API_REGISTER_ENDPOINT             = "http://localhost:8080/v1/hosts"
-    API_CPU_MEASUREMENTS_ENDPOINT     = "http://localhost:8080/v1/metrics/CPU_testHost/measurements"
-    API_BATTERY_MEASUREMENTS_ENDPOINT = "http://localhost:8080/v1/metrics/Battery_testHost/measurements"
-
     token = "xxxxxxxx"
     register_header = { "access-token":token }
 
-    def __init__(self, metric):
+    def __init__(self, metric, sensor_name):
         self.metric = metric
-        self.set_variables()
+        self.set_variables(sensor_name)
     
-    def set_variables(self):
-        self.hostID = "pokoj 272A"
-        self.os     = "Windows"
+    def set_variables(self, sensor_name):
+        self.API_REGISTER_ENDPOINT             = "http://localhost:8080/v1/hosts"
+        self.API_CPU_MEASUREMENTS_ENDPOINT     = "http://localhost:8080/v1/metrics/" + sensor_name + "_CPU_Host/measurements"
+        self.API_BATTERY_MEASUREMENTS_ENDPOINT = "http://localhost:8080/v1/metrics/" + sensor_name + "_Battery_Host/measurements"
+        
+        self.hostID = sensor_name
+        self.os     = platform.system()
         if self.metric == "CPU":
             self.type           = "CPU"
             self.unit           = "%"
-            self.metric_id      = "CPU_testHost"
+            self.metric_id      = sensor_name + "_CPU_Host"
             self.collected_data = []
             self.timestamp      = []
         elif self.metric == "Battery":
             self.type           = "Battery"
             self.unit           = "%"
-            self.metric_id      = "Battery_testHost"
+            self.metric_id      = sensor_name + "_Battery_Host"
             self.collected_data = []
             self.timestamp      = []
         else:
             self.type1           = "CPU"
             self.unit1           = "%"
-            self.metric_id1      = "CPU_testHost"
+            self.metric_id1      = sensor_name + "_CPU_Host"
             self.type2           = "Battery"
             self.unit2           = "%"
-            self.metric_id2      = "Battery_testHost"
+            self.metric_id2      = sensor_name + "_Battery_Host"
             self.collected_data1 = []
             self.collected_data2 = []
             self.timestamp       = []
@@ -65,7 +66,6 @@ class SensorTools:
             except requests.exceptions.ConnectionError:
                 print("Can not find server. Try to connect again after 5 seconds...")
                 time.sleep(5)
-
     
     def register_json_data_for_one_metric(self):
         data            = {}
@@ -114,13 +114,13 @@ class SensorTools:
             self.collected_data2 = []
         elif self.metric == "CPU":
             data = self.json_data_for_one_metric()
-            r = requests.post(url = self.API_CPU_MEASUREMENTS_ENDPOINT, json = sdata)#, params = register_header)
+            r = requests.post(url = self.API_CPU_MEASUREMENTS_ENDPOINT, json = data)#, params = register_header)
             print(data)
             print(r.status_code, r.reason)
             self.collected_data = []
         else:
             data = self.json_data_for_one_metric()
-            r = requests.post(url = self.API_BATTERY_MEASUREMENTS_ENDPOINT, json = sdata)#, params = register_header)
+            r = requests.post(url = self.API_BATTERY_MEASUREMENTS_ENDPOINT, json = data)#, params = register_header)
             print(data)
             print(r.status_code, r.reason)
             self.collected_data = []
