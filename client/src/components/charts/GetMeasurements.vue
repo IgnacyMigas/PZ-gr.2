634c2e1ad2b3d8d04c1371fa8963eeb06cf55bd7
tomasz.kakol <tomasz.kakol@elmodis.com>
2019-05-29 16:54:39 +0200

    retrive data from http response

--- client/src/components/charts/GetMeasurements.vue
+++ client/src/components/charts/GetMeasurements.vue
@@ -1,62 +1,41 @@
 <template>
-    <div id="app">
-        <h1>Test - retrive data</h1>
-
-        <section v-if="errored">
-            <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
-        </section>
-
-        <section v-else>
-            <div v-if="loading">Loading...</div>
-
-            <div class="currency">
-                {{ currency.description }}:
-                <span class="lighten">
-        <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
-      </span>
-            </div>
-        </section>
+    <div id="show-blogs">
+        <h1>All Blog Articles</h1>
+        <div v-for="blog in blogs" class="single-blog">
+            <h2>{{ blog.title }}</h2>
+            <article>{{ blog.body }}</article>
+        </div>
     </div>
 </template>
 
 
+
 <script>
-    import axios from 'axios';
-
-    // window.axios.defaults.headers.common = {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json'
-    // };
-
     export default {
-        name: 'Measurements',
-        data: function() {
+        data () {
             return {
-                info: null,
-                loading: true,
-                errored: false
+                blogs: []
             }
         },
-        filters: {
-            currencydecimal (value) {
-                return value.toFixed(2)
-            }
+        methods: {
         },
-        mounted () {
-            axios
-                .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-                .then(response => {
-                    this.info = response.data.bpi
-                })
-                .catch(error => {
-                    //console.log(error)
-                    this.errored = true
-                })
-                .finally(() => this.loading = false)
+        created() {
+            this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
+                this.blogs = data.body.slice(0,10);
+            });
         }
     }
 </script>
 
-<style scoped>
-
+<style>
+    #show-blogs{
+        max-width: 800px;
+        margin: 0px auto;
+    }
+    .single-blog{
+        padding: 20px;
+        margin: 20px 0;
+        box-sizing: border-box;
+        background: #eee;
+    }
 </style>
