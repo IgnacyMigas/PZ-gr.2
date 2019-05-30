34ac977e3e2f306af5e74b613d26cb7fa00dbf9d
tomasz.kakol <tomasz.kakol@elmodis.com>
2019-05-30 16:04:16 +0200

    retrive metrics data

--- client/src/components/charts/LinearChart.vue
+++ client/src/components/charts/LinearChart.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <h1><a @click="created">Show data</a></h1>
+        <h1><a @click="handleButtonClick" >Pokaż dane dla: {{ metricsName_2 }}</a></h1>
+        <h2>{{metrics}}</h2>
+        <h2>{{metrics_keys}}</h2>
+        <h2>{{monitor_keys }}</h2>
         <apexcharts ref="updateChart" height=350 align="left" type="line" :options="chartOptions" :series="series"></apexcharts>
         <!--<h2>{{ time }}</h2>-->
         <!--<h2>{{ value }}</h2>-->
@@ -13,10 +16,15 @@
     import VueApexCharts from 'vue-apexcharts'
 
     var host = 'http://localhost:8080'
-    var metricsName = 'Battery_testHost'
+    // var metricsName_1 = 'CPU_testHost'
+    var metricsName_2 = 'Battery_testHost'
+    //var metricsName_3 = 'CPU_Host'
     var version = 'v1'
     var n = 15
-    var url = host + '/' + version + '/metrics/' + metricsName + '/measurements?n=' + n
+
+
+    var url_metrics = host + '/' + version + '/metrics'
+    var url_2 = host + '/' + version + '/metrics/' + metricsName_2 + '/measurements?n=' + n
 
      var time= []
      var value = []
@@ -27,8 +35,19 @@
             apexcharts: VueApexCharts,
         },
         methods: {
+            getMetrics: function(){
+                this.$http.get(url_metrics , {useCredentails: true}).then(function (data) {
+                    this.metrics= data.body.metrics;
+
+                     var index;
+                     for (index = 0; index < this.metrics.length; ++index) {
+                         this.metrics_keys[index]  = this.metrics[index]["metric-id"];
+                         this.monitor_keys[index]  = this.metrics[index]["monitor-id"];
+                     }
+                });
+            },
             created: function() {
-                this.$http.get(url, {useCredentails: true}).then(function (data) {
+                this.$http.get(url_2, {useCredentails: true}).then(function (data) {
                     this.blob_samples = data.body.slice(0, 10);
 
                     var index;
@@ -47,6 +66,11 @@
                     })
                 });
             },
+            handleButtonClick: function() {
+                /* call two methods. */
+                this.getMetrics();
+                this.created();
+            }
         },
             data() {
                 return {
@@ -125,8 +149,12 @@
                     time: [],
                     value: [],
                     blob_samples: [],
-
+                    metricsName_2: metricsName_2,
+                    metrics: [],
+                    metrics_keys: [],
+                    monitor_keys : [],
                     mounted: function () {
+                        this.getMetrics()
                         this.created()
                     },
                 }
