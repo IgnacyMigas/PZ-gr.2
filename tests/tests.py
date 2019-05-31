054fa09c38cc5b1816d0d3d549ed9edd2492189c
Adam K <amkurzak@gmail.com>
2019-05-31 10:02:54 +0200

    travis

---
+++ tests/tests.py
@@ -0,0 +1,10 @@
+import unittest
+import requests as r
+
+class TestTest(unittest.TestCase):
+
+    def test_metrics(self):
+        self.assertEqual(True, True)
+
+if __name__ == '__main__':
+    unittest.main()
