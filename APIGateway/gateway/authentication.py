5460269ece98107d17256924fe56a4a4dab9d050
Ignacy Migas <gigi.migas@gmail.com>
2019-06-02 14:37:54 +0200

    Add Content-Type header

--- APIGateway/gateway/authentication.py
+++ APIGateway/gateway/authentication.py
@@ -4,7 +4,8 @@ from . import config
 
 def authorize(token):
     body = {'access-token': token}
-    response = requests.get(config.auth_url+config.method, params=body)
+    headers = {'Content-Type': 'application/json'}
+    response = requests.get(config.auth_url+config.method, headers=headers, params=body)
     if response.status_code == 200 and 'logged_in_as' in response.content:
         return True
     return False
--- APIGateway/gateway/config.py
+++ APIGateway/gateway/config.py
@@ -1,2 +1,2 @@
-auth_url = 'http://localhost:8000/'
+auth_url = 'http://127.0.0.1:5000/'
 method = 'v1/protected'
