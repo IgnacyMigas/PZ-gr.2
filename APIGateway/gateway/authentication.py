c85c25051eae6642968d07fe26fafc47e4dbc8c8
Ignacy Migas <gigi.migas@gmail.com>
2019-06-02 20:39:55 +0200

    Auth Service integration

--- APIGateway/gateway/authentication.py
+++ APIGateway/gateway/authentication.py
@@ -1,11 +1,12 @@
 import requests
 from . import config
+import json
 
 
 def authorize(token):
-    body = {'access-token': token}
-    headers = {'Content-Type': 'application/json'}
-    response = requests.get(config.auth_url+config.method, headers=headers, params=body)
-    if response.status_code == 200 and 'logged_in_as' in response.content:
+    body = json.dumps({'access_token': token})
+    headers = {'content-type': 'application/json'}
+    response = requests.get(config.auth_url+config.method, headers=headers, data=body)
+    if response.status_code == 200 and 'logged_in_as' in json.loads(response.content):
         return True
     return False
