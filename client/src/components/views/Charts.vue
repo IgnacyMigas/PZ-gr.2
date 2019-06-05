fb621d6ab99155953951302f9809cacb32251aeb
TomKak <tomaszkakol19921114@gmail.com>
2019-06-05 21:00:05 +0200

    add chart (TODO token)

--- client/src/components/views/Charts.vue
+++ client/src/components/views/Charts.vue
@@ -2,12 +2,7 @@
   <page title="Wykresy">
     <div id="container">
       <div id="chartsContainer">
-
         <LinearChart />
-        <BrushChart />
-        <RealtimeChart />
-        <!--<Measurements />-->
-
       </div>
 
     </div>
@@ -16,28 +11,22 @@
 </template>
 
 <script>
-    import Page from '@/components/templates/Page'
-    import LinearChart from '@/components/charts/LinearChart.vue'
-    import BrushChart from '@/components/charts/BrushChart.vue'
-    // import Measurements from '@/components/charts/GetMeasurements.vue'
-    import RealtimeChart from '@/components/charts/RealtimeChart.vue'
+  import Page from '@/components/templates/Page'
+  import LinearChart from '@/components/charts/LinearChart.vue'
 
-    /**@group Strony
-     * @vuese
-     * Strona prezentacji wykresów.
-     *
-     * Używana bezpośrednio przez router.
-     */
-    export default {
-        name: 'Charts',
-        components: {
-            'page': Page,
-            LinearChart,
-            BrushChart,
-            RealtimeChart
-            // ,Measurements
-        }
+  /**@group Strony
+   * @vuese
+   * Strona prezentacji wykresów.
+   *
+   * Używana bezpośrednio przez router.
+   */
+  export default {
+    name: 'Charts',
+    components: {
+      'page': Page,
+      LinearChart,
     }
+  }
 
 </script>
 
@@ -48,14 +37,7 @@
     min-height: 100px;
     margin: 0 auto
   }
-
   #chartsContainer {
     padding: 10px;
   }
-
-  .myChart {
-    margin-top: 30px;
-    padding: 40px;
-  }
-
 </Style>
